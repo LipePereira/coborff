ff52c3c7d79e15a2418411bdf96ef553eb57b0f2
LipePereira <fpereira@terpmail.umd.edu>
2015-04-10 21:33:55 -0300

    start!

--- application/libraries/facebook/Facebook.php
+++ application/libraries/facebook/Facebook.php
@@ -5,7 +5,7 @@ if ( session_status() == PHP_SESSION_NONE ) {
 }
 
 // Autoload the required files
-require_once( APPPATH . 'vendor/autoload.php' );
+require_once( base_url() . 'vendor/autoload.php' );
 
 use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookSession;
