ddcb1d1b2c36d171285122e73f55ea3002e55d24
LipePereira <fpereira@terpmail.umd.edu>
2015-04-10 23:10:37 -0300

    facebook shit

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('facebook/Facebook', 'session');
+$autoload['libraries'] = array('facebook/facebook', 'session');
 
 
 /*
