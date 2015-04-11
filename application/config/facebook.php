302a8e0f1f272d83c007eb52b2c89c1bd7cff873
LipePereira <fpereira@terpmail.umd.edu>
2015-04-11 14:00:25 -0300

    facebook shit

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,7 +1,7 @@
 <?php
 $config['facebook']['api_id'] = '271886533010703';
 $config['facebook']['app_secret'] = '3842c017581a9141c1f6b7bb214b4ba1';
-$config['facebook']['redirect_url'] = 'http://hoozup.us/login';
+$config['facebook']['redirect_url'] = 'http://hoozup.us/';
 $config['facebook']['permissions'] = array(
   'email',
 );
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,30 +1,35 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-            $this->load->view('coborff/start');
-            
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     * 	- or -
+     * 		http://example.com/index.php/welcome/index
+     * 	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see http://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index() {
+        $this->load->view('coborff/start');
+
+        $fb_user = $this->facebook->get_user();
+        $data["fb_user"] = $fb_user;
+        if (!$fb_user) {
             $data["login_url"] = $this->facebook->login_url();
-            
-            $this->load->view('welcome_message', $data);
-            $this->load->view('coborff/end');
-	}
+        }
+
+        $this->load->view('welcome_message', $data);
+        $this->load->view('coborff/end');
+    }
+
 }
