31cc696b62bca0eac99064f93d1d87aafa8eb779
LipePereira <fpereira@terpmail.umd.edu>
2015-04-11 13:55:20 -0300

    facebook shit

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -4,6 +4,4 @@ $config['facebook']['app_secret'] = '3842c017581a9141c1f6b7bb214b4ba1';
 $config['facebook']['redirect_url'] = 'http://hoozup.us/login';
 $config['facebook']['permissions'] = array(
   'email',
-  'user_location',
-  'user_birthday'
 );
